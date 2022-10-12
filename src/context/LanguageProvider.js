b9f44ae74c447ad595f53b29e198a07db2045b4a
Angelika <szczepanczykangelika@gmail.com>
2022-10-12 19:32:45 +0200

    update: language strings

--- src/context/LanguageProvider.js
+++ src/context/LanguageProvider.js
@@ -1,32 +1,32 @@
-import { createContext, useContext,  useEffect,  useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { languageOptions } from "../languages/options";
 
 export const initialState = {
-    language: 'en',
-    dictionary: languageOptions.english
-}
+  language: "English",
+  dictionary: languageOptions.english,
+};
 
-export const LanguageContext = createContext(initialState)
+export const LanguageContext = createContext(initialState);
 
 export const ContextProvider = ({ children }) => {
-  const [language, setLanguage] = useState('en');
+  const [language, setLanguage] = useState("English");
   const [dictionary, setDictionary] = useState(languageOptions.english);
-  
+
   const provider = {
     language,
-    changeLanguage: languageStr => {
-      setLanguage(languageStr)
-      if(languageStr=='pl'){
-        setDictionary(languageOptions.polish)
+    changeLanguage: (languageStr) => {
+      setLanguage(languageStr);
+      if (languageStr == "Polski") {
+        setDictionary(languageOptions.polish);
       } else {
-        setDictionary(languageOptions.english)
+        setDictionary(languageOptions.english);
       }
-    }
-  }
+    },
+  };
 
   return (
     <LanguageContext.Provider value={provider}>
       {children}
     </LanguageContext.Provider>
-  )};
-  
+  );
+};
