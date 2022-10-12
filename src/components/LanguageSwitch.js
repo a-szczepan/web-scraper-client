725ab16bcf7350cfe8d61b460e4e05b8b8f9262e
Angelika <szczepanczykangelika@gmail.com>
2022-10-12 19:34:16 +0200

    feat: add styled language switch toggle

---
+++ src/components/LanguageSwitch.js
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { styled } from "@mui/material/styles";
+import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { LanguageContext } from "../context/LanguageProvider";
+import PolishImg from "../assets/poland.svg";
+import EnglishImg from "../assets/uk.svg";
+
+const StyledSwitch = styled(Switch)(({ theme }) => ({
+  " .MuiSwitch-thumb": {
+    Image: `url(${PolishImg})`,
+  },
+  "	.MuiSwitch-thumb": {
+    Image: `url(${EnglishImg})`,
+  },
+}));
+
+export default function LanguageSwitch(props) {
+  const { language, changeLanguage } = useContext(LanguageContext);
+
+  return (
+    <div>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              defaultChecked
+              onChange={(e) => {
+                e.preventDefault();
+                changeLanguage(language === "English" ? "Polski" : "English");
+              }}
+            />
+          }
+          label={language}
+          sx={{
+            "& .MuiSwitch-thumb ": {
+              backgroundImage: `url(${
+                language === "English" ? EnglishImg : PolishImg
+              })`,
+            },
+            "& .MuiSwitch-track": {
+              backgroundColor: "primary.main",
+            },
+          }}
+        ></FormControlLabel>
+      </FormGroup>
+    </div>
+  );
+}
