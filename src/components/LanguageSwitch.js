a2be64b68ee55d4c14cf5378d74fa1b1ffe19b2e
Angelika <szczepanczykangelika@gmail.com>
2022-10-12 19:35:54 +0200

    update: delete unused import

--- src/components/LanguageSwitch.js
+++ src/components/LanguageSwitch.js
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { styled } from "@mui/material/styles";
 import Switch from "@mui/material/Switch";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
