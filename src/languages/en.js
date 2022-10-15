3507d9de7a90eff5d3eec922c7ab1d0166dad6ee
Angelika <szczepanczykangelika@gmail.com>
2022-10-15 08:23:34 +0200

    update: language strings

--- src/languages/en.js
+++ src/languages/en.js
@@ -1,14 +1,17 @@
 export default {
-    appDescription:  "",
-    switchLanguage: "",
-    searchPlaceholder: "",
-    filtersButton: "",
-    contain: "",
-    notContain: "",
-    addFilter: "",
-    moreFilters: "",
-    category: "",
-    brand: "",
-    choosePlaceholder: "",
-    ingredients: ""
-}
+  headerText: "Filtering the ingredients of products",
+  appAnnotationI: `This application was created by using MERN Stack technologies. For data scraping, the Puppeteer library was used. The project has only educational purposes.`,
+  appAnnotationII: `The database contains haircare products available on the web drugstore Rossmann. Not to disturb service work, the search range was reduced to only one product category. For the same reason, the web scraper isn't run automatically. All photos and product data are from Rossmann's website. After clicking on a product card, follow a redirection to the source website.`,
+  appDescription:
+    "The application lets filter products by containing particular ingredients. Additionally, a category and keywords can be included in the search.",
+  searchPlaceholder: "",
+  filtersButton: "",
+  contain: "",
+  notContain: "",
+  addFilter: "",
+  moreFilters: "",
+  category: "",
+  brand: "",
+  choosePlaceholder: "Choose",
+  ingredients: "Ingredients",
+};
