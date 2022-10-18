e45c1429549fcb0724032597d0fb3a7607dbdab4
Angelika <szczepanczykangelika@gmail.com>
2022-10-18 18:03:36 +0200

    refactor: cleanup

--- src/context/LanguageProvider.js
+++ src/context/LanguageProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { languageOptions } from "../languages/options";
 
 export const initialState = {
