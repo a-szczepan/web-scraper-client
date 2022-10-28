42c3ab424fb62014d16ba12c0978a11f0c917399
Angelika <szczepanczykangelika@gmail.com>
2022-10-28 11:46:15 +0200

    style: update page content display (depends on screen)

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import Box from "@mui/material/Box";
+import { useState, useEffect } from "react";
+import { Box } from "@mui/material";
 import INCIFilter from "./Filters";
 import Content from "./Content";
 import SearchBar from "./SearchBar";
+import DrawerBox from "./DrawerBox";
 
 export default function PageContent() {
-  const [useFilters, setUseFilters] = useState(false);
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
   const [filter, setFilter] = useState({
     keyWords: "",
     firstFilter: {
@@ -16,19 +17,52 @@ export default function PageContent() {
       contain: false,
       list: [],
     },
-    category: "",
+    category: false,
     brands: [],
   });
 
+  const update = () => {
+    setDesktop(window.innerWidth > 900);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", update);
+    return () => window.removeEventListener("resize", update);
+  });
+
   return (
-    <Box>
-      <SearchBar useFilters={useFilters} setUseFilters={setUseFilters} />
-      <Box sx={{ display: { sm: "flex" } }}>
-        {useFilters ? (
-          <INCIFilter filter={filter} setFilter={setFilter} />
-        ) : null}
-        <Content sx={{ flexBasis: "100%" }} />
-      </Box>
+    <Box sx={{ width: "100%", maxWidth: "1537px", alignSelf: "center" }}>
+      {isDesktop ? (
+        <Box sx={{ display: { sm: "flex" }, gap: "15px" }}>
+          <Box backgroundColor="coral" sx={{ flexBasis: "20%" }}>
+            <INCIFilter filter={filter} setFilter={setFilter} />
+          </Box>
+          <Box
+            backgroundColor="lightgreen"
+            flexBasis="80%"
+            display="flex"
+            flexDirection="column"
+            gap="10px"
+            pt="20px"
+          >
+            <SearchBar filter={filter} setFilter={setFilter} />
+            <Content sx={{ width: "100%" }} />
+          </Box>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: "5px",
+            p: "10px 0px 10px 0px",
+          }}
+        >
+          <DrawerBox filter={filter} setFilter={setFilter} />
+          <Content sx={{ width: "100%" }} />
+        </Box>
+      )}
     </Box>
   );
 }
