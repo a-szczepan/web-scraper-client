3f5a20525397181a18c2e2cd08f4a14d8b5c5945
Angelika <szczepanczykangelika@gmail.com>
2022-10-28 11:40:23 +0200

    feat: add search handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
 import { LanguageContext } from "../context/LanguageProvider";
 import { InputBase, Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 export default function SearchBar(props) {
   const { dictionary } = useContext(LanguageContext);
+  const [keyWords, setKeywords] = useState("");
+  const { handleSubmit } = useForm();
 
   return (
     <Paper
@@ -14,6 +17,9 @@ export default function SearchBar(props) {
         display: "flex",
         width: "50%",
       }}
+      onSubmit={handleSubmit(() =>
+        props.setFilter({ ...props.filter, keyWords })
+      )}
     >
       <InputBase
         placeholder={dictionary.searchPlaceholder}
@@ -23,8 +29,9 @@ export default function SearchBar(props) {
           flex: 1,
           caretColor: "black",
         }}
+        onChange={(e) => setKeywords(e.target.value)}
       />
-      <IconButton type="button">
+      <IconButton type="submit">
         <SearchIcon />
       </IconButton>
     </Paper>
