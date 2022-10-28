d16a8f8613b65761d3954b85c8ff49cac6739e20
Angelika <szczepanczykangelika@gmail.com>
2022-10-28 09:53:31 +0200

    style: change filters box display

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,6 +1,14 @@
 import { useContext, useState } from "react";
 import { LanguageContext } from "../context/LanguageProvider";
-import { Box, Button } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  List,
+  ListItem,
+  Divider,
+  IconButton,
+} from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import BrandFilter from "./BrandFilter";
 import CategoryFilter from "./CategoryFilter";
@@ -12,47 +20,63 @@ export default function Filters(props) {
   const [addExtraFilters, setAddExtraFilters] = useState(false);
 
   return (
-    <Box sx={{ backgroundColor: "green" }}>
-      <INCIFilter
-        key={1}
-        filter={props.filter}
-        setFilter={props.setFilter}
-        contain={contain}
-        setContain={setContain}
-        refs={{
-          type: "first",
-          list: props.filter.firstFilter.list,
-          registerContain: "containFirst",
-          registerIngredient: "ingredientFirst",
-        }}
-      />
-      <Button
-        variant="outlined"
-        onClick={() => setAddExtraFilters(!addExtraFilters)}
-        startIcon={<AddCircleIcon />}
-      >
-        {dictionary.addFilter}
-      </Button>
-      {addExtraFilters ? (
-        <INCIFilter
-          key={2}
-          filter={props.filter}
-          setFilter={props.setFilter}
-          contain={contain}
-          setContain={setContain}
-          refs={{
-            type: "second",
-            list: props.filter.secondFilter.list,
-            registerContain: "containSecond",
-            registerIngredient: "ingredientSecond",
-          }}
-        />
-      ) : null}
-      <Box>
-        <CategoryFilter filter={props.filter} setFilter={props.setFilter} />
-        <BrandFilter filter={props.filter} setFilter={props.setFilter} />
-      </Box>
-      <Button variant="contained">{dictionary.applyFilters}</Button>
+    <Box display="flex" flexDirection="column">
+      <List>
+        <ListItem>
+          <Box display="flex" flexDirection="column">
+            <Typography variant="h6" sx={{ m: "12px 0 12px 0" }}>
+              {dictionary.ingredients}
+            </Typography>
+            <INCIFilter
+              key={1}
+              filter={props.filter}
+              setFilter={props.setFilter}
+              contain={contain}
+              setContain={setContain}
+              refs={{
+                type: "first",
+                list: props.filter.firstFilter.list,
+                registerContain: "containFirst",
+                registerIngredient: "ingredientFirst",
+              }}
+            />
+            <Button
+              variant="text"
+              onClick={() => setAddExtraFilters(!addExtraFilters)}
+              startIcon={<AddCircleIcon />}
+              sx={{ alignSelf: "flex-start" }}
+            >
+              {dictionary.addFilter}
+            </Button>
+            {addExtraFilters ? (
+              <INCIFilter
+                key={2}
+                filter={props.filter}
+                setFilter={props.setFilter}
+                contain={contain}
+                setContain={setContain}
+                refs={{
+                  type: "second",
+                  list: props.filter.secondFilter.list,
+                  registerContain: "containSecond",
+                  registerIngredient: "ingredientSecond",
+                }}
+              />
+            ) : null}
+          </Box>
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <CategoryFilter filter={props.filter} setFilter={props.setFilter} />
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <BrandFilter filter={props.filter} setFilter={props.setFilter} />
+        </ListItem>
+        <ListItem>
+          <Button variant="contained">{dictionary.applyFilters}</Button>
+        </ListItem>
+      </List>
     </Box>
   );
 }
